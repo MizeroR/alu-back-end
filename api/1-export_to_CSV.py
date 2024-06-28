329e777153432b8e203665cc1afcd05d3ac74faf
MizeroR <r.mizero@alustudent.com>
2024-06-28 10:39:01 +0200

    changes in csv file

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+"""Exports to-do list info for a given employee ID to CSV format."""
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    USER_ID = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(USER_ID)).json()
-    USERNAME = user.get("USERNAME")
-    todos = requests.get(url + "todos", params={"userId": USER_ID}).json()
+    employee_id=sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todo="https://jsponplaceholder.typicode.com/todo?userIs={}"
+    todo=todo.format(employee_id)
 
-    with open("{}.csv".format(USER_ID), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [USER_ID, USERNAME, t.get("TASK_COMPLETED_STATUS"), t.get("TASK_TITLE")]
-         ) for t in todos]
+    user_info = requests.request("GET",url).json()
+    todo_info = requests.request("GET",todo).json()
+
+    employee_name=user_info.get("name")
+    total_tasks=list(filter(lambda x:(x["completed"] is True),todo_info))
+    task_com=len(total_tasks)
+    total_task_done=len(todo_info)
+
+    with open(str(employee_id) + '.csv' , "w") as f:
+        [f.writer('"' + str(employee_id) + '",'+
+                  '"' + employee_username + '",'+
+                  '"' + str(task["completed"]) + '",'+
+                  '"' + task("title") + '",'+ "\n")
+            
+          for task in todo_info]
