d4a850a1fba422f896b330d52cf8f15a55790819
MizeroR <r.mizero@alustudent.com>
2024-06-28 11:08:15 +0200

    changes

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -5,10 +5,11 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    employee_id=sys.argv[1]
+    employee_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo="https://jsponplaceholder.typicode.com/todo?userId={}"
-    todo=todo.format(employee_id)
+
+    todo = "https://jsponplaceholder.typicode.com/todo?userId={}"
+    todo = todo.format(employee_id)
 
     user_info = requests.request("GET",url).json()
     todo_info = requests.request("GET",todo).json()
@@ -19,6 +20,6 @@ if __name__ == "__main__":
         dict(zip(["task", "completed", "username"],
                   [task["title"], task["completed", employee_username]]))
                   for task in todo_info]
-    user_dict={str(employee_id):todos_info_sorted}
+    user_dict = {str(employee_id):todos_info_sorted}
     with open(str(employee_id) + 'json', "w") as f:
               f.write(json.dumps(user_dict))
